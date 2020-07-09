d4cb88f69563a524e4665a7e5a0477c1f6e30f3a
Bryan Silva <brandonb2816@gmail.com>
2020-07-09 20:14:56 -0300

    Leyendo el presupuesto del formulario

--- src/Components/Pregunta.js
+++ src/Components/Pregunta.js
@@ -1,6 +1,15 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Pregunta = () => {
+
+    // Definir el state de la cantidad
+    const [cantidad, guardarCantidad] = useState(0);
+
+    // Funcion que lee el presupuesto
+    const definirPresupuesto = e => {
+        guardarCantidad( parseInt(e.target.value), 10 );
+    }
+    
     return ( 
         <Fragment>
             <h2>Coloca tu presupuesto</h2>
@@ -10,6 +19,7 @@ const Pregunta = () => {
                     type="number"
                     className="u-full-width"
                     placeholder="Coloca tu presupuesto"
+                    onChange={definirPresupuesto}
                 />
 
                 <input 
